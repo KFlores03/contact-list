d73e994f8df2330924a63481b511f8976853cd32
KFlores03 <kevin21flores@gmail.com>
2023-07-31 16:47:09 -0600

    Creada la funcion para actualizar un contacto

--- project-2.js
+++ project-2.js
@@ -36,3 +36,32 @@ console.log(listaContactos); // Mostrar la lista de contactos en la consola
 
 eliminarContacto(1); // Eliminar el contacto con ID 1
 console.log(listaContactos); // Mostrar la lista actualizada de contactos en la consola
+
+function actualizarContacto(id, nuevosDatos) {
+  // Buscar el índice del contacto con el ID dado
+  const indiceContacto = listaContactos.findIndex(contacto => contacto.id === id);
+
+  // Si el contacto con el ID dado existe, actualizar sus datos
+  if (indiceContacto !== -1) {
+    // Actualizar los campos del contacto con los nuevos datos
+    listaContactos[indiceContacto].nombres = nuevosDatos.nombres;
+    listaContactos[indiceContacto].apellidos = nuevosDatos.apellidos;
+    listaContactos[indiceContacto].telefono = nuevosDatos.telefono;
+    listaContactos[indiceContacto].ubicacion = nuevosDatos.ubicacion;
+    listaContactos[indiceContacto].ciudad = nuevosDatos.ciudad;
+    listaContactos[indiceContacto].direccion = nuevosDatos.direccion;
+  }
+}
+
+// Ejemplo de uso:
+// Supongamos que tenemos un contacto con ID 1 y queremos actualizar sus datos
+actualizarContacto(1, {
+  nombres: "Nuevo Nombre",
+  apellidos: "Nuevo Apellido",
+  telefono: "987654321",
+  ubicacion: "Nueva Ubicación",
+  ciudad: "Nueva Ciudad",
+  direccion: "Nueva Dirección"
+});
+
+console.log(listaContactos);
